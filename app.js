ef246770496553ae3c0933d3cf6ba1f25c01e0d7
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-06-24 17:30:52 +0530

    javascript-into

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log("console, World!");
+
+let name = "World";
+
+const address = "panadura";
+
+// console.log( address);
+
+let nameArray = ["saman", "sunil", "nimal","kamal"];
+
+// console.log(nameArray[3]);
+
+// for (let i = 0; i < nameArray.length; i++) {
+//     console.log(nameArray[i]);
+// }
+
+// for(let name of nameArray){
+//     console.log(name);
+// }
+
+// let index = 0;
+// while(index<5){
+//     console.log(nameArray[index]);
+//     index++;
+// }
+
---
+++ app.ts
@@ -0,0 +1,5 @@
+class example{
+    name: string = "kesh";
+    age: number = 20;
+    isAdd: boolean = true;
+}
